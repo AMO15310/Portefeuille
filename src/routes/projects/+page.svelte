f4c0d65fd8b04b90647d9e1ecb57575a5de41cdf
AMO15310 <wachiraamos402@gmail.com>
2023-08-20 00:00:34 +0300

    feat:projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,4 +1,52 @@
-<script></script>
+<script>
+	import musicPlayerImg from '../../assets/music-player.png';
+	import ecomImg from '../../assets/e-commerce.png';
+	import waterAppImg from '../../assets/benmax-bucket.png';
+	import medinfoImg from '../../assets/medinfoplus.jpg';
+	import phonebookImg from '../../assets/phonebook.png';
+	import watertrackerImg from '../../assets/water-tracker-v2.png';
+</script>
+
 <main>
-    
+	<div class="h-[100vh] w-[100vw] bg-blue-400 flex flex-col">
+		<div class="flex justify-center text-white mt-7 text-xl font-semibold">Some Of My Projects</div>
+		<div class="flex flex-wrap justify-center p-10">
+			<div class="w-1/3 p-2">
+				<div class="h-[14rem] bg-green-400">
+					<img src={musicPlayerImg} class="h-[100%] w-[100%]" alt="music" />
+				</div>
+				<p class="title text-center mt-2 text-white">Music Player</p>
+			</div>
+			<div class="w-1/3 p-2">
+				<div class="h-[14rem] bg-green-400">
+					<img src={ecomImg} class="h-[100%] w-[100%]" alt="music" />
+				</div>
+				<p class="title text-center mt-2 text-white">E-commerce App</p>
+			</div>
+			<div class="w-1/3 p-2">
+				<div class="h-[14rem] bg-green-400">
+					<img src={waterAppImg} class="h-[100%] w-[100%]" alt="music" />
+				</div>
+				<p class="title text-center mt-2 text-white">Water Tracker</p>
+			</div>
+			<div class="w-1/3 p-2">
+				<div class="h-[14rem] bg-green-400">
+					<img src={medinfoImg} class="h-[100%] w-[100%]" alt="music" />
+				</div>
+				<p class="title text-center mt-2 text-white">Health Information Web</p>
+			</div>
+			<div class="w-1/3 p-2">
+				<div class="h-[14rem] bg-green-400">
+					<img src={phonebookImg} class="h-[100%] w-[100%]" alt="music" />
+				</div>
+				<p class="title text-center mt-2 text-white">Phonebook</p>
+			</div>
+			<div class="w-1/3 p-2">
+				<div class="h-[14rem] bg-green-400">
+					<img src={watertrackerImg} class="h-[100%] w-[100%]" alt="music" />
+				</div>
+				<p class="title text-center mt-2 text-white">Water App V1</p>
+			</div>
+		</div>
+	</div>
 </main>
